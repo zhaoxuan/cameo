a2864ad7f2ecd38aff11ff2c897ecbed0bad522b
zhaoxuan <zhaoxuan1727@gmail.com>
2012-08-28 16:01:06 +0800

    first project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cameo::Application.config.secret_token = '1ca25da24619a59f78ece9960508ba149993d85d143d2370f0310ca84962910145d2e57fe8f90eac00459861e5f15d465bc26ff5e3338429ceba1ada88ce1402'
